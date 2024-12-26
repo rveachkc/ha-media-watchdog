592ac20fed353922f09abff9cbd80d5adb9dd63c
rveach <rveach@gmail.com>
2024-12-26 01:02:18 -0600

    watchdog rules with intervals

--- src/ha_watchdog_libs/watchdog_rules.py
+++ src/ha_watchdog_libs/watchdog_rules.py
@@ -1,13 +1,16 @@
 from typing import Self, Literal, Optional
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
+from ha_watchdog_libs.watchdog_intervals import WatchdogInterval
 
 @dataclass
 class WatchdogRule:
     name: str
     action: Literal["warn", "stop", "home"]
-    sources: Optional[list[str]] = None
-    sources_except: Optional[list[str]] = None
-    entity_ids: Optional[list[str]] = None
+    sources: Optional[list[str]] = field(default_factory=list)
+    sources_except: Optional[list[str]] = field(default_factory=list)
+    entity_ids: Optional[list[str]] = field(default_factory=list)
+    intervals: Optional[list[WatchdogInterval]] = field(default_factory=list)
 
     def rule_applies(self: Self, entity_id: str, source_name: str) -> bool:
 
@@ -18,5 +21,11 @@ class WatchdogRule:
                 True if not self.entity_ids else (entity_id in self.entity_ids),
                 True if not self.sources else (source_name in self.sources),
                 True if not self.sources_except else (source_name not in self.sources_except),
+                True if not self.intervals else any(
+                    map(
+                        lambda x: x.is_active(),
+                        self.intervals,
+                    )
+                )
             ]
         )
---
+++ tests/test_watchdog_interval_rules.py
@@ -0,0 +1,94 @@
+from typing import Self
+from unittest import TestCase
+
+from freezegun import freeze_time
+
+from ha_watchdog_libs.watchdog_rules import WatchdogRule
+from ha_watchdog_libs.watchdog_intervals import WatchdogInterval
+
+class TestIntervalRule(TestCase):
+
+    KIDS_TV = "media_player.kids_watch_this_tv"
+    WIND_UP_SHOWS = "Funny Stuff that'll keep them laughing all night"
+    CALM_SHOWS = "Boring Stuff to help sleep"
+
+    wr = WatchdogRule(
+        name="TestIntervalRule",
+        action="warn",
+        sources=[WIND_UP_SHOWS],
+        intervals=[
+            WatchdogInterval(
+                start_time="21:00",
+                end_time="24:00",
+                days=["Sun", "Mon", "Tue", "Wed", "Thu"],
+            ),
+            WatchdogInterval(
+                start_time="22:00",
+                end_time="24:00",
+                days=["Friday", "Saturday"],
+            )
+        ]
+    )
+
+    @freeze_time("2024-12-26 12:30")
+    def test_thu_early(self: Self):
+
+        self.assertFalse(
+            self.wr.rule_applies(entity_id=self.KIDS_TV, source_name=self.WIND_UP_SHOWS)
+        )
+        self.assertFalse(
+            self.wr.rule_applies(entity_id=self.KIDS_TV, source_name=self.CALM_SHOWS)
+        )
+
+
+    @freeze_time("2024-12-26 21:30")
+    def test_thu_late(self: Self):
+
+        self.assertTrue(
+            self.wr.rule_applies(entity_id=self.KIDS_TV, source_name=self.WIND_UP_SHOWS)
+        )
+        self.assertFalse(
+            self.wr.rule_applies(entity_id=self.KIDS_TV, source_name=self.CALM_SHOWS)
+        )
+
+    @freeze_time("2024-12-26 23:30")
+    def test_thu_really_late(self: Self):
+
+        self.assertTrue(
+            self.wr.rule_applies(entity_id=self.KIDS_TV, source_name=self.WIND_UP_SHOWS)
+        )
+        self.assertFalse(
+            self.wr.rule_applies(entity_id=self.KIDS_TV, source_name=self.CALM_SHOWS)
+        )
+
+
+    @freeze_time("2024-12-27 12:30")
+    def test_fri_early(self: Self):
+
+        self.assertFalse(
+            self.wr.rule_applies(entity_id=self.KIDS_TV, source_name=self.WIND_UP_SHOWS)
+        )
+        self.assertFalse(
+            self.wr.rule_applies(entity_id=self.KIDS_TV, source_name=self.CALM_SHOWS)
+        )
+
+
+    @freeze_time("2024-12-27 21:30")
+    def test_fri_late(self: Self):
+
+        self.assertFalse(
+            self.wr.rule_applies(entity_id=self.KIDS_TV, source_name=self.WIND_UP_SHOWS)
+        )
+        self.assertFalse(
+            self.wr.rule_applies(entity_id=self.KIDS_TV, source_name=self.CALM_SHOWS)
+        )
+
+    @freeze_time("2024-12-27 23:30")
+    def test_fri_really_late(self: Self):
+
+        self.assertTrue(
+            self.wr.rule_applies(entity_id=self.KIDS_TV, source_name=self.WIND_UP_SHOWS)
+        )
+        self.assertFalse(
+            self.wr.rule_applies(entity_id=self.KIDS_TV, source_name=self.CALM_SHOWS)
+        )
